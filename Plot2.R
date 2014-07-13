b85d8107d83ccc5214e6b9f205a52848c0bff0f1
gcfogleman <gcfogleman@users.noreply.github.com>
2014-07-13 15:48:47 -0400

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,16 @@
+##  Coursera Exploratory Data Analysis Project 1, Plot 2
+
+## Read the power consumption table
+HWdata <- read.table("household_power_consumption.txt", header=TRUE, sep = ";")
+## Create tables for 1/2/2007 and 2/2/2007
+HWdataSub1 <- HWdata[HWdata$Date == "1/2/2007",]
+HWdataSub2 <- HWdata[HWdata$Date == "2/2/2007",]
+## Combine the two subsets into a single dataframe
+## This is the dataframe that contains data to be plotted
+HWdataSub <- rbind(HWdataSub1, HWdataSub2)
+
+## create png file and save histogram to that file:
+png(file = "plot2.png")
+## default is 480 pixels by 480 pixels
+plot(as.numeric(HWdataSub$Global_active_power), ylab = "Global Active Power (kilowatts)", type = "l")
+dev.off()
